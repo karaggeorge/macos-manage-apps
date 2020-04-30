900ed428abb506ca2c08a4dd2b7e439367736bd2
Sindre Sorhus <sindresorhus@gmail.com>
2020-04-30 13:05:20 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,26 +2,26 @@
 import PackageDescription
 
 let package = Package(
-	name: "macos-launch-application",
-	platforms: [
-		.macOS(.v10_12)
-	],
-	products: [
-		.executable(
-			name: "launch-app",
-			targets: [
-				"LaunchApp"
-			]
-		),
+  name: "macos-launch-application",
+  platforms: [
+    .macOS(.v10_12)
+  ],
+  products: [
     .executable(
-			name: "kill-app",
-			targets: [
-				"KillApp"
-			]
-		)
-	],
-	targets: [
-		.target(name: "LaunchApp"),
+      name: "launch-app",
+      targets: [
+        "LaunchApp"
+      ]
+    ),
+    .executable(
+      name: "kill-app",
+      targets: [
+        "KillApp"
+      ]
+    )
+  ],
+  targets: [
+    .target(name: "LaunchApp"),
     .target(name: "KillApp")
-	]
+  ]
 )
