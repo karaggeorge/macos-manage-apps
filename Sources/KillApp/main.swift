501e80f81e8455ecfe01773cb090224bcb857b8b
Sindre Sorhus <sindresorhus@gmail.com>
2020-04-30 13:09:58 +0800

    Update main.swift

--- Sources/KillApp/main.swift
+++ Sources/KillApp/main.swift
@@ -1,7 +1,7 @@
 import Cocoa
 
 guard let bundleId = Array(CommandLine.arguments.dropFirst(1)).first else {
-  print("Please provide the bundle identifier")
+  print("Please specify a bundle identifier")
   exit(1)
 }
 
